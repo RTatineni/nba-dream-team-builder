eba32d058057c41e52563176431897b1859961fc
Rajiv <rtatineni2@gmail.com>
2019-06-14 17:52:40 -0500

    fixed up player_id and added rest of teams

--- assets/javascript/logo-view.js
+++ assets/javascript/logo-view.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+  $("#back-btn").hide()
   var logos = [
     "./assets/images/atlantahawks.png",
     "./assets/images/bostonceltics.png",
@@ -12,10 +12,27 @@ $(document).ready(function() {
     "./assets/images/detroitpistons.png",
     "./assets/images/goldenstatewarriors.png",
     "./assets/images/houstonrockets.png",
-    "./assets/images/indianapacers.png"
-
+    "./assets/images/indianapacers.png",
+    "./assets/images/losangelesclippers.png",
+    "./assets/images/losangeleslakers.png",
+    "./assets/images/memphisgrizzlies.png",
+    "./assets/images/miamiheat.png",
+    "./assets/images/milwaukeebucks.png",
+    "./assets/images/minnesotatimberwolves.png",
+    "./assets/images/neworleanspelicans.png",
+    "./assets/images/newyorkknicks.png",
+    "./assets/images/okcthunder.png",
+    "./assets/images/orlandomagic.png",
+    "./assets/images/philadelphia76ers.png",
+    "./assets/images/phoenixsuns.png",
+    "./assets/images/portlandtrailblazers.png",
+    "./assets/images/sacramentokings.png",
+    "./assets/images/sanantoniospurs.png",
+    "./assets/images/torontoraptors.png",
+    "./assets/images/utahjazz.png",
+    "./assets/images/washingtonwizards.png"
   ];
-  var team_names = ["atlantahawks", "bostonceltics", "brooklynnets","char","chic","cle","dallas","denver","detroit","golden","hous","indiana"];
+  var team_names = ["atlantahawks", "bostonceltics", "brooklynnets","char","chic","cle","dallas","denver","detroit","golden","hous","indiana","clippers","losangeleslakers","memphisgrizzlies","miami","milwaukeebucks","minnesotatimberwolves","neworleanspelicans","newyorkknicks","okcthunder","orlandomagic","philadelphia","pheonix","portland","sacremento","sanantoniospurs","torontoraptors","utah","washington"];
   for (i in logos) {
     var logo ="<img src=" +logos[i] +" class='png'" +"data-name=" +team_names[i] +" width='100px'" +">";
     $("#logo-holder").append(logo);
@@ -38,25 +55,43 @@ $(document).ready(function() {
     }).then(function(response) {
         for(i in response){
             //$("#roster-holder").append(response[i].name + "<br>")
-            var player = "<a class='player'" +"data-name=" +response[i].name + "data-id="+i +" width='100px'" +">"+response[i].name + "<a><br>"
+            var player = "<a class='player'" +"data-name=" +response[i].name + " data-id="+i+" width='100px'" +">"+response[i].name + "<a><br>"
             $("#roster-holder").append(player);
-
             //console.log(response[i].name)
         }
-        $(document).on("click", ".player", function() {
+        $(document).on("click", ".player", function(e) {
             $("#roster-holder").hide()
-            console.log(response[$(this).data("id")])
-            $("#player-card").append("g: "+ response[$(this).data("id")].games_played + "<br>")
-            $("#player-card").append("fg%: "+ response[$(this).data("id")].field_goal_percentage + "<br>")
-            $("#player-card").append("ft%: "+ response[$(this).data("id")].free_throw_percentage + "<br>")
-            $("#player-card").append("rpg: "+ response[$(this).data("id")].rebounds_per_game + "<br>")
-            $("#player-card").append("apg: "+ response[$(this).data("id")].assists_per_game + "<br>")
-            $("#player-card").append("spg: "+ response[$(this).data("id")].steals_per_game + "<br>")
-            $("#player-card").append("bpg: "+ response[$(this).data("id")].blocks_per_game + "<br>")
-            $("#player-card").append("ppg: "+ response[$(this).data("id")].points_per_game + "<br>")
-
+            player_id = $(this).data("id")
+            getPlayerCard(player_id, team.substring(0,3))
+            console.log(player_id)
         });
 
     });
+  }
+
+
+  function getPlayerCard(player_id, team){
+      var queryURL = "https://nba-players.herokuapp.com/players-stats-teams/"+team;
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+      }).then(function(response){
+
+          console.log(player_id)
+        $("#player-card").append("g: "+ response[player_id].games_played + "<br>")
+        $("#player-card").append("fg%: "+ response[player_id].field_goal_percentage + "<br>")
+        $("#player-card").append("ft%: "+ response[player_id].free_throw_percentage + "<br>")
+        $("#player-card").append("rpg: "+ response[player_id].rebounds_per_game + "<br>")
+        $("#player-card").append("apg: "+ response[player_id].assists_per_game + "<br>")
+        $("#player-card").append("spg: "+ response[player_id].steals_per_game + "<br>")
+        $("#player-card").append("bpg: "+ response[player_id].blocks_per_game + "<br>")
+        $("#player-card").append("ppg: "+ response[player_id].points_per_game + "<br>")
+      })
+
+      $(document).on("click","#back-btn",function(){
+        $("#back-btn").hide()
+        getPlayerRoster()
+      })
+      
   }
 });
