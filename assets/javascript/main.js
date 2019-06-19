c51a8561d29b6bd3cbb97aaf5985760a04b34476
Rajiv <rtatineni2@gmail.com>
2019-06-19 09:39:45 -0500

    added code to read nested data from database

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,105 +1,111 @@
 // Initializing Firebase
 
 $(document).ready(function() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyASNxFpWnUnEbpKox3TQKajO_UH5pyFHS8",
+    authDomain: "nba-page.firebaseapp.com",
+    databaseURL: "https://nba-page.firebaseio.com",
+    projectId: "nba-page",
+    storageBucket: "nba-page.appspot.com",
+    messagingSenderId: "75407839994",
+    appId: "1:75407839994:web:f0498ddbbc807e28"
+  };
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDjquF_i-ha4twszQrxG09zFCZWXjEDMsc",
-  authDomain: "nba-dream-team-f5550.firebaseapp.com",
-  databaseURL: "https://nba-dream-team-f5550.firebaseio.com",
-  projectId: "nba-dream-team-f5550",
-  storageBucket: "nba-dream-team-f5550.appspot.com",
-  messagingSenderId: "847958678823",
-  appId: "1:847958678823:web:c9f3e25ddf001643"
-};
+  firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
 
-var database = firebase.database();
+  //Code for Authorization user to firebase//
 
+  // const auth = firebase.auth();
 
+  // const promise = auth.signInWithEmailAndPassword(email, pass);
+  // auth.createUserWithEmailAndPassword(email, pass);
 
-//Code for Authorization user to firebase//
+  //method which listens to authentication changes- sign in/out//
+  // firebase.auth().onAuthStateChanged()
 
-// const auth = firebase.auth();
+  //Register First time users with username and password//
 
-// const promise = auth.signInWithEmailAndPassword(email, pass);
-// auth.createUserWithEmailAndPassword(email, pass);
+  document.getElementById("btnSignUp").addEventListener("click", function(e) {
+    $("#results").empty();
 
-//method which listens to authentication changes- sign in/out//
-// firebase.auth().onAuthStateChanged()
+    console.log("button works");
+    const email = document.getElementById("txtEmail").value;
+    console.log(email);
+    // ***Need to validate email*** //
 
-//Register First time users with username and password//
+    const pass = document.getElementById("txtPassword").value;
+    console.log(pass);
+    const auth = firebase.auth();
+    // ***Need to validate Psw- 6 character length min ***/
+    // Validate length
 
-document.getElementById("btnSignUp").addEventListener('click', function(e) {
-  $("#results").empty();
+    if (pass.length < 6) {
+      $("#results").html("Password must contain: Minimum 6 characters");
+    }
 
-  console.log("button works");
-  const email = document.getElementById("txtEmail").value;
-  console.log(email);
-// ***Need to validate email*** //
-
-  const pass = document.getElementById("txtPassword").value;
-  console.log(pass);
-  const auth = firebase.auth();
-// ***Need to validate Psw- 6 character length min ***/
- // Validate length
-
- if(pass.length < 6) {
-$("#results").html("Password must contain: Minimum 6 characters");
-} 
- 
-// Create user with email and psw in firebase//
-  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-  
-    $("#results").html(error.message);
-  
-
+    // Create user with email and psw in firebase//
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, pass)
+      .catch(function(error) {
+        $("#results").html(error.message);
+      });
   });
-});
 
-//Sign in returning user with username and password//
+  var email;
+  //Sign in returning user with username and password//
+  document.getElementById("btnLogin").addEventListener("click", e => {
+    email = document.getElementById("txtEmail").value;
+    const pass = document.getElementById("txtPassword").value;
+    const auth = firebase.auth();
+    const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
 
-document.getElementById("btnLogin").addEventListener('click', e=>{
+    console.log("Logging in");
+    $("#results").html("Loggin in");
+
+    if(email != null){
+      database.ref("/user_rosters/" + email.substring(0,3)).on("child_added", function(childSnapshot) {
+        //console.log(childSnapshot.val().player_name)
+        $("#dream-roster").append("<br>"+childSnapshot.val().player_name + "<br>")
+        //console.log(player_name)
   
-  const email = document.getElementById("txtEmail").value;
-  const pass = document.getElementById("txtPassword").value;
-  const auth = firebase.auth();
-  const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
+      })
+    }
+
+
+    promise.catch(e => {
+      $("#results").html(e.massage);
+    });
+  });
+
+  //Acting upon state change (Sign in/Sign out)/
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      document.getElementById("btnLogOut").classList.remove("hide");
+      $("#results").html("Welcome Back User!");
+     
+      console.log(user);
+    } else {
+      document.getElementById("btnLogOut").classList.add("hide");
+      $("#results").html("Not Logged In!");
+    }
+  });
+
+  //Logging out//
+
+  document.getElementById("btnLogOut").addEventListener("click", e => {
+    firebase.auth().signOut();
+
+    console.log("logged out");
+    $("#results").html("Logged out");
+  });
+
+  $("#back-btn-card").hide();
+  $("#back-btn-roster").hide();
+  $("#add-to-roster-btn").hide();
   
-console.log("Logging in");
-$("#results").html("Loggin in");
-
-
-  promise.catch(e=>{ $("#results").html(e.massage)})
-});
-
-
- //Acting upon state change (Sign in/Sign out)/
-firebase.auth().onAuthStateChanged(user=>{ 
-  if(user){
-    document.getElementById("btnLogOut").classList.remove('hide')
-    $("#results").html("Welcome Back User!");
-    console.log(user);
-  } else{
-    document.getElementById("btnLogOut").classList.add('hide')
-    $("#results").html("Not Logged In!")
-  }
-})
-
-//Logging out//
-
-document.getElementById("btnLogOut").addEventListener('click', e=>{
-  firebase.auth().signOut();
- 
-  console.log('logged out');
-  $("#results").html("Logged out");
-
-})
-
-
-
-  $("#back-btn-card").hide()
-  $("#back-btn-roster").hide()
   var logos = [
     "./assets/images/atlantahawks.png",
     "./assets/images/bostonceltics.png",
@@ -133,113 +139,201 @@ document.getElementById("btnLogOut").addEventListener('click', e=>{
     "./assets/images/washingtonwizards.png",
     "./assets/images/nbalogo.png"
   ];
-  
+
   // Loop through the teams and display each team logo on the screen.
-  var team_names = ["atlantahawks", "bostonceltics", "brooklynnets","char","chic","cle","dallas","denver","detroit","gsw","hous","indiana","laclippers","lal","memphisgrizzlies","miami","milwaukeebucks","minnesotatimberwolves","neworleanspelicans","newyorkknicks","okcthunder","orlandomagic","philadelphia","pho","portland","sacremento","sana","torontoraptors","utahjazz","washington"];
+  var team_names = [
+    "atlantahawks",
+    "bostonceltics",
+    "brooklynnets",
+    "char",
+    "chic",
+    "cle",
+    "dallas",
+    "denver",
+    "detroit",
+    "gsw",
+    "hous",
+    "indiana",
+    "laclippers",
+    "lal",
+    "memphisgrizzlies",
+    "miami",
+    "milwaukeebucks",
+    "minnesotatimberwolves",
+    "neworleanspelicans",
+    "newyorkknicks",
+    "okcthunder",
+    "orlandomagic",
+    "philadelphia",
+    "pho",
+    "portland",
+    "sacremento",
+    "sana",
+    "torontoraptors",
+    "utahjazz",
+    "washington"
+  ];
   for (i in logos) {
-    var logo ="<img src=" +logos[i] +" class='png'" +"data-name=" +team_names[i] +" width='100px'" +">";
+    var logo =
+      "<img src=" +
+      logos[i] +
+      " class='png'" +
+      "data-name=" +
+      team_names[i] +
+      " width='100px'" +
+      ">";
     $("#logo-holder").append(logo);
   }
-// Inputs: 
-    //team: the team name for the player selected
+
+
+
+  // Inputs:
+  //team: the team name for the player selected
   // Calls the api for acquiring team roster appends each player on the roster to the screen.
   function getPlayerRoster(team) {
-    var queryURL ="https://nba-players.herokuapp.com/players-stats-teams/" +team.substring(0, 3);
+    var queryURL =
+      "https://nba-players.herokuapp.com/players-stats-teams/" +
+      team.substring(0, 3);
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        $("#back-btn-roster").show()
-    
-        for(i in response){
-            var player = "<a class='player'" +"data-name='"+response[i].name+"' data-team="+team+" data-id='"+i+"' width='100px'" +">"+response[i].name + "<a><br>"
-            console.log(player);
-            $("#roster-holder").append(player);
-        }
+      $("#back-btn-roster").show();
+
+      for (i in response) {
+        var player =
+          "<a class='player'" +
+          "data-name='" +
+          response[i].name +
+          "' data-team=" +
+          team +
+          " data-id='" +
+          i +
+          "' width='100px'" +
+          ">" +
+          response[i].name +
+          "<a><br>";
+        $("#roster-holder").append(player);
+      }
     });
   }
-  // Inputs: 
-    //player_id: the id that the player coresponds to in the roster array.
-    //team: the team name for the player selected
-    //url : a url for getting the player headshot
+  // Inputs:
+  //player_id: the id that the player coresponds to in the roster array.
+  //team: the team name for the player selected
+  //url : a url for getting the player headshot
   // Calls the api for acquiring stats for a specific player, and appends the stats as well as the picture to the screen.
 
-  function getPlayerCard(player_id, team,url){
-      var queryURL = "https://nba-players.herokuapp.com/players-stats-teams/"+team;
-      $.ajax({
-          url: queryURL,
-          method: "GET",
-          
-      }).then(function(response){
-        console.log(url)
-        $("#back-btn-card").show()
-        $("#player-card").html("<img src="+url+ " width='300px'><br>")
-        $("#player-card").append("g: "+ response[player_id].games_played + "<br>")
-        $("#player-card").append("fg%: "+ response[player_id].field_goal_percentage + "<br>")
-        $("#player-card").append("ft%: "+ response[player_id].free_throw_percentage + "<br>")
-        $("#player-card").append("rpg: "+ response[player_id].rebounds_per_game + "<br>")
-        $("#player-card").append("apg: "+ response[player_id].assists_per_game + "<br>")
-        $("#player-card").append("spg: "+ response[player_id].steals_per_game + "<br>")
-        $("#player-card").append("bpg: "+ response[player_id].blocks_per_game + "<br>")
-        $("#player-card").append("ppg: "+ response[player_id].points_per_game + "<br>")
-      })
+  var g;
+  var fg;
+  var ft;
+  var rpg;
+  var apg;
+  var spg;
+  var bpg;
+  var ppg;
+  var url_link;
+  var position;
+  var player_name;
+
+  function getPlayerCard(player_id, team, url) {
+    var queryURL =
+      "https://nba-players.herokuapp.com/players-stats-teams/" + team;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(url);
+      url_link = url;
+      g = response[player_id].games_played;
+      fg = response[player_id].field_goal_percentage;
+      ft = response[player_id].free_throw_percentage;
+      rpg = response[player_id].rebounds_per_game;
+      apg = response[player_id].assists_per_game;
+      spg = response[player_id].steals_per_game;
+      bpg = response[player_id].blocks_per_game;
+      ppg = response[player_id].points_per_game;
+      $("#back-btn-card").show();
+      $("#add-to-roster-btn").show()
+      $("#player-card").html("<img src=" + url + " width='300px'><br>");
+      $("#player-card").append("g: " + g + "<br>");
+      $("#player-card").append("fg%: " + fg + "<br>");
+      // $("#player-card").append("fg%: "+ response[player_id].field_goal_percentage + "<br>")
+      $("#player-card").append("ft%: " + ft + "<br>");
+      $("#player-card").append("rpg: " + rpg + "<br>");
+      $("#player-card").append("apg: " + apg + "<br>");
+      $("#player-card").append("spg: " + spg + "<br>");
+      $("#player-card").append("bpg: " + bpg + "<br>");
+      $("#player-card").append("ppg: " + ppg + "<br>");
+    });
   }
-// Search Feature
+  // Search Feature
 
+  function getPlayerPosition(name) {
+    var queryURL = "https://www.balldontlie.io/api/v1/players/?search=" + name;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      position = response.data[0].position;
+    });
+  }
 
+  $("#add-to-roster-btn").on("click", function(event) {
+    event.preventDefault();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    database.ref("user_rosters/" + email.substring(0, 3)).push({
+      player_name: player_name,
+      position: position,
+      url_link: url_link,
+      g: g,
+      fg: fg,
+      ft: ft,
+      rp: rpg,
+      apg: apg,
+      spg: spg,
+      bpg: bpg,
+      ppg: ppg
+    });
+  });
   // When user selects a specific logo, call the GetPlayerRoster function. and hide logos
   $(document).on("click", ".png", function() {
     $("#logo-holder").hide();
     getPlayerRoster($(this).data("name"));
   });
 
+
+
   // When user selects a specific player in roster screen acquire playercard by passing in variables of player and team.
   $(document).on("click", ".player", function(e) {
-    e.preventDefault()
-    var name = $(this).data("name").split(" ")
-    console.log(name)
-    var team = $(this).data("team")
-    var url = 'https://nba-players.herokuapp.com/players/'+name[1]+'/'+name[0];
-    $("#player-card").empty()
-    getPlayerCard($(this).data("id"), team.substring(0,3),url)
-    $("#back-btn-roster").hide()
-    $("#roster-holder").hide()
+    e.preventDefault();
 
+    var name = $(this)
+      .data("name")
+      .split(" ");
+    console.log(name);
+    var team = $(this).data("team");
+    var url =
+      "https://nba-players.herokuapp.com/players/" + name[1] + "/" + name[0];
+    player_name = $(this).data("name");
+    $("#player-card").empty();
+    getPlayerCard($(this).data("id"), team.substring(0, 3), url);
+    getPlayerPosition($(this).data("name"));
+    $("#back-btn-roster").hide();
+    $("#roster-holder").hide();
   });
-// When user selects back go back to logos by hiding this back button and showing logos
-  $(document).on("click","#back-btn-roster",function(e){
-      e.preventDefault()
-      $("#back-btn-roster").hide()
-      $("#logo-holder").show()
-      $("#roster-holder").empty()
-  })
-// When user slects back go back to roster screen and empty before so that new player can be rendered.
-  $(document).on("click","#back-btn-card",function(e){
-    e.preventDefault()
-    $("#back-btn-card").hide()
-    $("#roster-holder").show()
-    $("#back-btn-roster").show()
-    $("#player-card").empty()
-    
-  })
-
+  // When user selects back go back to logos by hiding this back button and showing logos
+  $(document).on("click", "#back-btn-roster", function(e) {
+    e.preventDefault();
+    $("#back-btn-roster").hide();
+    $("#logo-holder").show();
+    $("#roster-holder").empty();
+  });
+  // When user slects back go back to roster screen and empty before so that new player can be rendered.
+  $(document).on("click", "#back-btn-card", function(e) {
+    e.preventDefault();
+    $("#back-btn-card").hide();
+    $("#roster-holder").show();
+    $("#back-btn-roster").show();
+    $("#player-card").empty();
+  });
 });
